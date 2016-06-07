a0ee2e6b0b5b7f3801f5bfa2f0b2aa4abb3f7706
Adam Boyd <adamboyd50@gmail.com>
2016-06-06 19:09:36 -0700

    crashlytics + fabric

--- DownTube/DownTube/AppDelegate.swift
+++ DownTube/DownTube/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import CoreData
+import Fabric
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        Fabric.with([Crashlytics.self])
+        
         return true
     }
 
